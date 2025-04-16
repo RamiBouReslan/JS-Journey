94d1b7a35c22a7ea1e5de930f0827550f8838b41
Rb-112594 <reboureslan@gmail.com>
2025-04-16 12:56:14 +0300

    Defining the functionality of Add Task button.

---
+++ scripts/toDoList.js
@@ -0,0 +1,15 @@
+const input = document.getElementById('taskInput');
+const addButton = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+
+addButton.addEventListener('click', function () {
+  const taskText = input.value.trim();
+
+  if (taskText !== "") {
+    const li = document.createElement('li');
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+    input.value = "";
+  }
+});
