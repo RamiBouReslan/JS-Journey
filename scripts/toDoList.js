54cec3072f785823c42eb693ef8c18d036d6f556
Rb-112594 <reboureslan@gmail.com>
2025-04-16 13:10:01 +0300

    Making functionality to the tasks added

--- scripts/toDoList.js
+++ scripts/toDoList.js
@@ -1,15 +1,19 @@
-const input = document.getElementById('taskInput');
-const addButton = document.getElementById('addBtn');
-const taskList = document.getElementById('taskList');
+const input = document.getElementById("taskInput");
+const addButton = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
 
-addButton.addEventListener('click', function () {
+addButton.addEventListener("click", function () {
   const taskText = input.value.trim();
 
   if (taskText !== "") {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.textContent = taskText;
 
     taskList.appendChild(li);
     input.value = "";
+
+    li.addEventListener("click", function () {
+      li.classList.toggle("completed");
+    });
   }
 });
